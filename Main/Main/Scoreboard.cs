bd56ed9c7440aac561c76ab45da0c96a65448fbd
Lars <lars@roth-kl.de>
2017-03-03 20:34:59 +0100

    Update Scoreboard.cs

--- Main/Main/Scoreboard.cs
+++ Main/Main/Scoreboard.cs
@@ -21,11 +21,15 @@ namespace Main
         private void btn_Submit_Click(object sender, EventArgs e)
         {
             //.pushBoard
+            // Read the csv File and remove the header and compare the data with the preloaded ArrayList and add new Entries.
+            // Convert the Csv Reads to a String-Array
+            // Paste the new StringArray Parts to the existing list-view at the scoreboard
         }
 
         private void btn_Refresh_Click(object sender, EventArgs e)
         {
             //.getBoard
+            // Push Data from Variables to a csv File with an predefinded Header
         }
     }
 }
